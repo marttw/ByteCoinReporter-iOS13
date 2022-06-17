b3d6e5614d21e5f589dbda27062aa107dcc09625
Martiniano <mortvillalba@gmail.com>
2022-06-17 11:30:16 -0300

    Bytecoin app completed

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Martiniano Villalba on 15/06/2022.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData : Codable {
+  let rate: Double
+}
